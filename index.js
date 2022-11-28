a4b553d0e8c9b18d2ba5402f1c025b541afa577f
nikolamirilo <nikolamirilo@gmail.com>
2022-11-28 13:26:32 +0100

    changed cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+let cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();

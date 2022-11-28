9e7f668edeaf3e4cf3b6addf7cac61deb93c784b
nikolamirilo <nikolamirilo@gmail.com>
2022-11-28 10:11:32 +0100

    optimized for deploy

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 let cors = require("cors");
@@ -5,9 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const routes = require("./routes/routes");
 
-mongoose.connect(
-  "mongodb+srv://nikola:react123@main.iresckc.mongodb.net/database?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 db.on("error", (err) => {
@@ -20,7 +19,7 @@ db.once("open", () => {
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/accounts", routes);
-
-app.listen(5000, () => {
-  console.log("Server listening on port 5000");
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
